a1e48f0135b7ec2b1afa7557f8d1349ecf5dbe57
mkreut <143921149+mkreut@users.noreply.github.com>
2024-02-22 16:38:32 -0700

    started changing text on start page, removed extra button

--- ui_accumulator.py
+++ ui_accumulator.py
@@ -56,28 +56,18 @@ class StartPage(tk.Frame):
 		tk.Frame.__init__(self, parent)
 		
 		# label of frame Layout 2
-		label = ttk.Label(self, text ="Startpage", font = LARGEFONT)
+		label = ttk.Label(self, text ="Welcome to UVSim!", font = LARGEFONT, justify='center')
 		
 		# putting the grid in its place by using
 		# grid
 		label.grid(row = 0, column = 4, padx = 10, pady = 10) 
 
-		button1 = ttk.Button(self, text ="Page 1",
+		button1 = ttk.Button(self, text ="Choose a file",
 		command = lambda : controller.show_frame(Page1))
 	
 		# putting the button in its place by
 		# using grid
 		button1.grid(row = 1, column = 1, padx = 10, pady = 10)
-
-		## button to show frame 2 with text layout2
-		button2 = ttk.Button(self, text ="Page 2",
-		command = lambda : controller.show_frame(Page2))
-	
-		# putting the button in its place by
-		# using grid
-		button2.grid(row = 2, column = 1, padx = 10, pady = 10)
-
-		
 
 
 # second window frame page1 
